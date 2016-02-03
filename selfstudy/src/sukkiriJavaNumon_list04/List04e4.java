0bae30ff1b2a13b14a8baac9039b14eef636c453
MIHO <MIHO@DESKTOP-TFMKKAD>
2016-02-03 18:37:53 +0900

    スッキリjava

---
+++ selfstudy/src/sukkiriJavaNumon_list04/List04e4.java
@@ -0,0 +1,23 @@
+package sukkiriJavaNumon_list04;
+
+public class List04e4 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		//(1)配列の準備
+		int[] numbers ={ 3, 4, 9 };
+
+		//(2)メッセージの表示
+		System.out.println("1桁の数字を入力してください");
+
+		//(3)キーボードからの数字入力
+		int input = new java.util.Scanner(System.in).nextInt();
+
+		//(4)配列を回しながら判別
+		for(int n : numbers){
+			if(n == input){
+				System.out.println("アタリ！");
+			}
+		}
+	}
+}
